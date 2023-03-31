0603416f1064e40cea41c62ed0c154aaeffec077
phongcampboy <phongcampboy@hotmail.com>
2023-03-31 15:56:06 +0700

    tap menu

--- app_flutter/lib/screen/home.dart
+++ app_flutter/lib/screen/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_new
 
 import 'package:flutter/material.dart';
 
@@ -15,9 +15,15 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        elevation: 0,
+        backgroundColor: Colors.blueAccent,
       ),
-      body: Center(
-        child: const Text('Home'),
+      body: Container(
+        height: 70,
+        decoration: new BoxDecoration(color: Colors.blueAccent),
+        child: new Center(
+          child: new Text("Hello, World!"),
+        ),
       ),
     );
   }
